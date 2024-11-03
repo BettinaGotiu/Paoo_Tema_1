67ebf9a4ea561f6f113a3750a123980982c5d9f8
Gotiu Bettina <bettinagotiu@gmail.com>
2024-11-03 20:40:29 +0200

    constructor

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,9 @@
 #include "Avion.hpp"
 
 int main() {
-    
+    //Avion folosind constructorul
     Avion avion1("Diamond DA42", 4, 356, "EFA");
-
-    // getter && details
-    std::cout << "Avion 1 - Nume: " << avion1.getNume() 
-              << ", Capacitate Locuri: " << avion1.getCapacitateLocuri() 
-              << ", Viteza Maxima: " << avion1.getVitezaMaxima() 
-              << ", Proprietar: " << avion1.getProprietar() << std::endl;
-
-    // setter for modifications
-    avion1.setProprietar("Lufthansa");
-    std::cout << "Avion 1 nou proprietar: " << avion1.getProprietar() << std::endl;
 
     return 0;
 }
+
